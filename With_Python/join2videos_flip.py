87612660142bfd76466983bf4673aa7e0f95ebca
Your Name <you@example.com>
2024-11-04 23:06:33 +0100

    adding reqs

--- With_Python/join2videos_flip.py
+++ With_Python/join2videos_flip.py
@@ -7,8 +7,8 @@ from moviepy.editor import VideoFileClip, concatenate_videoclips
 video_directory = "/media/jalcocert/BackUp/Z_BackUP_HD-SDD/OA5Pro/Z_Roma-29oct-2nov/ForoImperial/"
 
 # Load the videos
-video1 = VideoFileClip(video_directory + "DJI_20241030132520_0026_D.MP4")
-video2 = VideoFileClip(video_directory + "DJI_20241030145603_0036_D.MP4")
+video1 = VideoFileClip(video_directory + "DJI_20241030144545_0034_D.MP4")
+video2 = VideoFileClip(video_directory + "DJI_20241030145541_0035_D.MP4")
 
 # Flip the second video 180 degrees
 video2_flipped = video2.rotate(180)
@@ -18,32 +18,3 @@ final_video = concatenate_videoclips([video1, video2_flipped])
 
 # Write the result to a file
 final_video.write_videofile("output_video2.mp4", codec="libx264")
-
-
-# from moviepy.editor import VideoFileClip, concatenate_videoclips
-
-# # Define the directory where the videos are located
-# video_directory = "/media/jalcocert/BackUp/Z_BackUP_HD-SDD/OA5Pro/Z_Roma-29oct-2nov/ForoImperial/"
-
-# # Load the videos
-# video1 = VideoFileClip(video_directory + "DJI_20241030132520_0026_D.MP4")
-# video2 = VideoFileClip(video_directory + "DJI_20241030145603_0036_D.MP4")
-
-# # Concatenate the videos
-# final_video = concatenate_videoclips([video1, video2])
-
-# # Write the result to a file
-# final_video.write_videofile("output_video.mp4", codec="libx264")
-
-
-# # from moviepy.editor import VideoFileClip, concatenate_videoclips
-
-# # # Load the videos
-# # video1 = VideoFileClip("video1.mp4")
-# # video2 = VideoFileClip("video2.mp4")
-
-# # # Concatenate the videos
-# # final_video = concatenate_videoclips([video1, video2])
-
-# # # Write the result to a file
-# # final_video.write_videofile("output_video.mp4", codec="libx264")
