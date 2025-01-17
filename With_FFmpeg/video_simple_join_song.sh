92d82d006d87d227b317afe27563f441e595497c
JAlcocerT <contact@jalcocertech.xyz>
2025-01-17 17:57:49 +0100

    trim and song OK

--- With_FFmpeg/video_simple_join_song.sh
+++ With_FFmpeg/video_simple_join_song.sh
@@ -2,10 +2,16 @@ ls *.MP4 | sed "s/^/file '/; s/$/'/" > file_list.txt #add .mp4 of current folder
 
 #du -sh ./* #check their size
 
-#generate a video with them
+#generate a video from few parts
 #ffmpeg -f concat -safe 0 -i file_list.txt -c copy output_video.mp4
 ffmpeg -f concat -safe 0 -i file_list.txt -c:v copy -an silenced_output_video.mp4 #silenced video
 #ffmpeg -i output_video.mp4 -filter:v "setpts=PTS/4" -an fast_output_video.mp4 #
+
+##cut a video (trim)
+ffmpeg -i DJI_20250117120043_0014_D.MP4 -t 00:05:05 -c copy -avoid_negative_ts make_zero output.mp4
+#trim and add song
+ffmpeg -i DJI_20250117120043_0014_D.MP4 -i Cumulus\ Nimbus\ -\ Quincas\ Moreira.mp3 -t 00:05:05 -c:v copy -c:a aac -strict experimental -avoid_negative_ts make_zero output_with_audio.mp4
+
 
 #ffmpeg -stream_loop -1 -i "AETHER - Density & Time.mp3" -i silenced_output_video.mp4 -c:v copy -c:a aac -shortest output_with_song.mp4
 ffmpeg -stream_loop -1 -i "TRAVELATOR - Density & Time.mp3" -i silenced_output_video.mp4 -c:v copy -c:a aac -shortest output_with_song.mp4
