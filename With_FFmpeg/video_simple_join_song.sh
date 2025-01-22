b9f699d836e9ecd5c65792564b92437d9dcf2ed2
JAlcocerT <contact@jalcocertech.xyz>
2025-01-22 13:16:20 +0100

    extracting frames OK with ffmpeg

--- With_FFmpeg/video_simple_join_song.sh
+++ With_FFmpeg/video_simple_join_song.sh
@@ -13,5 +13,12 @@ ffmpeg -i DJI_20250117120043_0014_D.MP4 -t 00:05:05 -c copy -avoid_negative_ts m
 ffmpeg -i DJI_20250117120043_0014_D.MP4 -i Cumulus\ Nimbus\ -\ Quincas\ Moreira.mp3 -t 00:05:05 -c:v copy -c:a aac -strict experimental -avoid_negative_ts make_zero output_with_audio.mp4
 
 
+###take photo snapshots from video (to a folder)
+#ffmpeg -ss 00:00:34 -i DJI_20250116075915_0042_D.MP4 -frames:v 10 frame_%04d.jpg
+ffmpeg -ss 00:02:02 -i DJI_20250116075915_0042_D.MP4 -frames:v 60 ./crash/frame_%04d.jpg
+
+
 #ffmpeg -stream_loop -1 -i "AETHER - Density & Time.mp3" -i silenced_output_video.mp4 -c:v copy -c:a aac -shortest output_with_song.mp4
 ffmpeg -stream_loop -1 -i "TRAVELATOR - Density & Time.mp3" -i silenced_output_video.mp4 -c:v copy -c:a aac -shortest output_with_song.mp4
+
+### 🎵 Music by: 
